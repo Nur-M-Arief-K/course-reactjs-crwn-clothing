b4897d009bf92014f1c89de012445e24a46c50e6
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-10-26 16:32:55 +0700

    lesson 77: adding sass

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss'
+
 const App = () => {
   const categories = [
     {
@@ -28,7 +30,7 @@ const App = () => {
         categories.map(({id, title}) => 
           (
             <div className="category-container" key={id}>
-            <div className="background-image" />
+              <div className="background-image" />
               <div className="category-body-container">
                 <h2>{title}</h2>
                 <p>Shop Now</p>
