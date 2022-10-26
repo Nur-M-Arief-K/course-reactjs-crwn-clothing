1e6959e064c8b6653b7334be154b9e4516955f4f
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-10-26 21:39:14 +0700

    lesson 84: react router outlet
    testing react router outlet in home component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 
+const Shop = () => {
+  return (
+    <div>
+      <h1>Hi I'm the shop page</h1>
+    </div>
+  )
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />}>
+        <Route path="shop" element={<Shop />}/>
+      </Route>
     </Routes>
   ) 
 }
