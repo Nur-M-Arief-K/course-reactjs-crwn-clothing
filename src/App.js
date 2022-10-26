c4182bd381a8d03c8e16c3a4f9e8c5b1c22c597c
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-10-26 21:49:44 +0700

    lesson 85: navigation bar component
    setup navigation component in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 
-const Shop = () => {
+const Navigation = () => {
   return (
     <div>
-      <h1>Hi I'm the shop page</h1>
+      <div>
+        <h1>Hi I'm the navigation bar</h1>
+      </div>
+      <Outlet />
     </div>
   )
-};
+}
+
+const Shop = () => <h1>Hi I'm the shop page</h1>;
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />}/>
       </Route>
     </Routes>
