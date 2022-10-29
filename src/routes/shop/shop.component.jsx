3b2b826dd028e8bcc5a44f96fcefcec89fbbe92f
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-10-29 22:28:59 +0700

    lesson 132: category prev component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,6 @@
-import { useContext, Fragment } from "react";
+import { useContext } from "react";
 
-import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 import { CategoriesContext } from "../../contexts/categories.context";
 
@@ -9,22 +9,15 @@ import "./shop.component.scss"
 const Shop = () => {
     const {categoriesMap} = useContext(CategoriesContext); //categoriesMap is categoryMap {hats: [], jackets: [], etc}
     return (
-        <Fragment >
+        <div className="shop-container" >
             {
-                Object.keys(categoriesMap).map(title => ( //Object.keys is iterating keys of an object, NOT INCLUDING ITS VALUE!
-                    <Fragment key={title}>
-                        <h2>{title}</h2>
-                        <div className="products-container">
-                            {categoriesMap[title].map((product) => (
-                                    <ProductCard key={product.id} product={product} />
-                                ))
-                            }
-                        </div>
-                    </Fragment>
-                ))
+                Object.keys(categoriesMap).map(title => { //Object.keys is iterating keys of an object, NOT INCLUDING ITS VALUE!
+                    const products = categoriesMap[title]; //returning the keys and its value
+                    return <CategoryPreview key={title} title={title} products={products} />
+                })
             }
             
-        </Fragment>
+        </div>
     );
 };
 
