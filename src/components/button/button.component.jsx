f103bce91d12c901697e36ef4e4c5d430fdf1d4e
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-10-30 15:09:41 +0700

    lesson 137: styled-components-button
    migrate button.styles from scss to jsx, make adjustment toward it

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,12 +1,23 @@
-import "./button.styles.scss";
+import {BaseButton, GoogleSignInButton, InvertedButton} from "./button.styles.jsx";
 
-const BUTTON_TYPE_CLASSES = {
+export const BUTTON_TYPE_CLASSES = {
+    base: "base",
     google: "google-sign-in",
     inverted: "inverted"
 };
 
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
+    //THIS IS A METHOD TO TAKE A VALUE INSIDE AN OBJECT
+    {
+        [BUTTON_TYPE_CLASSES.base] : BaseButton,
+        [BUTTON_TYPE_CLASSES.google] : GoogleSignInButton,
+        [BUTTON_TYPE_CLASSES.inverted] : InvertedButton
+    }[buttonType] 
+);
+
 const Button = ({children, buttonType, ...otherProps}) => {
-    return <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} {...otherProps}>{children}</button>
+    const CustomButton = getButton(buttonType); //will return BaseButton/GoogleSignInButton/InvertedButton
+    return <CustomButton {...otherProps}>{children}</CustomButton>
 };
 
 export default Button;
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import { signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from "../..
 //signInAuthUserWithEmailAndPassword for sign in with email and password
 
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 
 import "./sign-in-form.styles.scss";
 
@@ -70,7 +70,7 @@ const SignInForm = () => {
 
                 <div className="buttons-container">
                     <Button type="submit">Sign in</Button>
-                    <Button type="button" buttonType="google" onClick={signInWithGoogle}>Google sign in</Button>
+                    <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google sign in</Button>
                 </div>
             </form>
         </div>
