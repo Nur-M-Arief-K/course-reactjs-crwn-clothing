d8e9d02062811f33462930e0c0a6266e4555241e
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-11-02 19:50:56 +0700

    lesson 167: redux-persist
    install redux-persist and leverage it inside store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 // import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
@@ -18,9 +21,23 @@ const loggerMiddleWare = (store) => (next) => (action) => {
     console.log("next state: ", store.getState()); //.getState() is function special in redux
 };
 
+//REDUX-PERSIST CONFIG START
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//REDUX-PERSIST CONFIG END
+
 const middleWares = [loggerMiddleWare];
 
 const composeEhancers = compose(applyMiddleware(...middleWares));
 
 //the second arg is setter for redux default state
-export const store = createStore(rootReducer, undefined, composeEhancers);
+export const store = createStore(persistedReducer, undefined, composeEhancers);
+
+export const persistor = persistStore(store);
