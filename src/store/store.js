d23e0fa41fe7674c88190bdf79faed92e326afc0
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-11-02 21:11:28 +0700

    lesson 169: async redux - redux thunk
    install redux thunk

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { compose, createStore, applyMiddleware } from "redux";
 import {persistStore, persistReducer} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
@@ -18,7 +18,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 //REDUX-PERSIST CONFIG END
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(Boolean);
 
 const composeEnhancers = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const composedEhancers = composeEnhancers(applyMiddleware(...middleWares));
