675c7a18d3d6bece7a4d2c03e9c4a30d3e8dd0df
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-11-02 13:29:20 +0700

    lesson 162: reselect library
    install reselect, leverage it inside category.selector.js

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,26 @@
-export const selectCategoriesMap = (state) => state.categories.categories
-.reduce((acc, category) => { 
-        const {title, items} = category;
-        acc[title.toLowerCase()] = items; //is a method to add new prop with its value to an object
-        return acc;
-    }, {});
+import {createSelector} from "reselect";
+
+//select categories prop (out of 2 props) from combine reducer inside store.js
+const selectCategoryReducer = (state) => state.categories;
+
+//memoization #1
+export const selectCategories = createSelector(
+    //input, if this different, run the code below
+    [selectCategoryReducer],
+    //output
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+//memoization #2
+export const selectCategoriesMap = createSelector(
+    //input, if this different, run the code below
+    [selectCategories],
+
+    //output
+    (categories) => {
+        return categories.reduce((acc, category) => { 
+            const {title, items} = category;
+            acc[title.toLowerCase()] = items; //is a method to add new prop with its value to an object
+            return acc;
+        }, {})}
+);
