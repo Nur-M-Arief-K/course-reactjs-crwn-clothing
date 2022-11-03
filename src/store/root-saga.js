6b2ef54a4ddf4b7308c53b9c04f6b3c4d2df59ad
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-11-03 14:07:22 +0700

    lesson 172: async redux redux-saga
    install redux-saga, create root-saga.js

---
+++ src/store/root-saga.js
@@ -0,0 +1,5 @@
+//REDUX-SAGA SETUP
+import {all, call} from "redux-saga/effects";
+
+//A generator funciton
+export function* rootSaga() {};
--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,9 @@ import { compose, createStore, applyMiddleware } from "redux";
 import {persistStore, persistReducer} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+//redux-saga replace redux-thunk
+import createSagaMiddleware from "@redux-saga/core";
+import { rootSaga } from "./root-saga";
 
 import { rootReducer } from "./root-reducer";
 
@@ -18,7 +20,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 //REDUX-PERSIST CONFIG END
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(Boolean);
+//REDUX-SAGA MIDDLEWARE SETUP
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [process.env.NODE_ENV !== "production" && logger, sagaMiddleware].filter(Boolean);
 
 const composeEnhancers = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const composedEhancers = composeEnhancers(applyMiddleware(...middleWares));
@@ -26,4 +31,8 @@ const composedEhancers = composeEnhancers(applyMiddleware(...middleWares));
 //the second arg is setter for redux default state
 export const store = createStore(persistedReducer, undefined, composedEhancers);
 
+//REDUX-SAGA
+sagaMiddleware.run(rootSaga);
+
+//REDUX-PERSIST 
 export const persistor = persistStore(store);
