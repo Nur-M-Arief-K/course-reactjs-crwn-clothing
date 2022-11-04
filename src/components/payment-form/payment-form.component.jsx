349682f3d669c906c92b85a84d4f3467c1ab7f57
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-11-04 18:05:20 +0700

    lesson 185: write serverless function

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,15 +1,31 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 
+import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+
 const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const paymentHandler = async (event) => {
+        event.preventDefault();
+
+        if(!stripe || !elements) return;
+
+
+    }
+
     return (
-        <div>
-            <CardElement />
-            <Button buttonType={BUTTON_TYPE_CLASSES.inverted} >
+        <PaymentFormContainer>
+            <FormContainer>
+                <h2>Credit card payment: </h2>
+                <CardElement />
+                <Button buttonType={BUTTON_TYPE_CLASSES.inverted} >
                 Pay Now
-            </Button>
-        </div>
+                </Button>
+            </FormContainer>
+        </PaymentFormContainer>
     )
 };
 
